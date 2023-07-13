021816eefd25987c24dfc8e269284a3daefa587c
Daniel "Sonk <admin@sonk.es>
2023-07-13 05:43:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,69 @@
 from bs4 import BeautifulSoup
-import requests
+from requests import get
 import socks
 import socket
 
-url = 'https://whoer.net'
+def get_ip(response):
+    if response.status_code == 200:
+        return response.text.replace(' ', '').rstrip()
+    else:
+        raise
+
+url = 'https://api.ipify.org/?format=txt'
+
+def check_country():
+    # Realizar la solicitud GET
+    try:
+        response = get("https://www.cual-es-mi-ip.net/geolocalizar-ip-mapa")
+    except:
+        return None
+
+    # Verificar si la solicitud fue exitosa (código de respuesta 200)
+    if response.status_code == 200:
+        # Crear el objeto BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Buscar elementos con la clase específica
+        elements = soup.find_all(class_='table table-striped')
+
+        # Imprimir los elementos encontrados
+        pais = str(str(elements).split('<td>País</td>')[1].split('</strong></td>')[0].replace('<td><strong>', '').replace("\n", ""))
+
+        return pais
+    else:
+        return None
 
 quest = input('¿Quieres usar tor? [S/n]: ')
 if quest == 's' or quest == 'S':
 # Configuración de SOCKS proxy TOR
     ports_list = [9150, 9050]
-    response = None
 
     for ports in ports_list:
         socks.set_default_proxy(socks.SOCKS5, "localhost", ports)
         socket.socket = socks.socksocket
         try:
-            response = requests.get(url)
+            response = get(url)
             break
         except Exception as e:
             pass
 
     # Comprobar si la solicitud fue exitosa
-    if response.status_code != 200 or response == None:
+    try:
+        if response.status_code != 200:
+            print('WARNING - RECUERDA QUE HAY QUE ABRIR TOR Y CONECTARSE PARA QUE PUEDA FUNCIONAR.')
+            print("ERROR - Error al hacer la solicitud HTTP GET a la página web.")
+            try:
+                print('ERROR - '+e)
+            except:
+                pass
+    except NameError:
         print('WARNING - RECUERDA QUE HAY QUE ABRIR TOR Y CONECTARSE PARA QUE PUEDA FUNCIONAR.')
         print("ERROR - Error al hacer la solicitud HTTP GET a la página web.")
 else:
     # Realizar la solicitud HTTP GET a la página web
-    import requests
-    response = requests.get(url)
-
-# Parsear el contenido HTML utilizando BeautifulSoup
-soup = BeautifulSoup(response.content, 'html.parser')
-
-# Encontrar los elementos HTML que contienen los nombres y precios de los productos
-ip_find = soup.find_all('div', class_="button_icon your-ip")
-
-# Iterar sobre los elementos encontrados e imprimir los nombres y precios de los productos
-for ip_data in ip_find:         
     try:
-        ip_sucio = ip_data.find('strong', class_="your-ip").text
-        ip = ip_sucio.replace('\n', '')
+        response = get(url)
+    except:
+        raise
 
-        print(ip)
-    except Exception as e:
-        print(e)
+print(f'{get_ip(response)} - {check_country()}')
